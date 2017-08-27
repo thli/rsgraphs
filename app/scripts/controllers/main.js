b794691c7273ea072d4e1f0f62f36d153452f925
Thomas Li <Thomas Li>
2017-08-27 12:41:04 -0400

    first deploy attempt

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rsgraphsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rsgraphsApp
+ */
+angular.module('rsgraphsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
